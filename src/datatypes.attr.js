32aee564706b6cb91b244b7e319d7ad13ffc1e33
Mark Gibson <jollytoad@gmail.com>
2009-05-21 11:45:16 +0100

    Moved attrToggle here from the jquery.roles project.

---
+++ src/datatypes.attr.js
@@ -0,0 +1,36 @@
+/*
+ * jQuery Datatypes - Attribute functions @VERSION (@DATE)
+ *
+ * Copyright (c) 2009 Adaptavist.com Ltd
+ * Dual licensed under the MIT (MIT-LICENSE.txt)
+ * and GPL (GPL-LICENSE.txt) licenses.
+ *
+ * Author: Mark Gibson (jollytoad at gmail dot com)
+ */
+/* Depends:
+ *   datatypes.core.js
+ */
+(function($) {
+
+$.fn.extend({
+
+// Toggle a boolean attribute state.
+// Only toggles if the state is already set, unless a default value is given.
+attrToggle: function( attr, def ) {
+	return this.each(function() {
+		var state = $.dt.bool( $.attr(this, attr) );
+
+		// Use the given default as the current state if not set yet
+		if ( state === undefined ) { state = def; }
+
+		// Only toggle the state if it is already set
+		if ( state !== undefined ) {
+			$.attr(this, attr, ""+!state);
+		}
+	});
+}
+
+});
+
+})(jQuery);
+
